b9542fa1b6089d3e45e49ec66cfe2d2763d03c13
Tus <pmtuss@gmail.com>
2022-08-25 14:28:27 +0700

    add validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   has_rich_text :content
   has_one_attached :image
   has_many :comments, -> { order "created_at DESC"}
+
+  validates :title, presence: true
+  validates :preview, presence: true
 end
